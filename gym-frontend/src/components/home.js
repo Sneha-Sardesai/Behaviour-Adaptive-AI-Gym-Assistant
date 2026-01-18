cede9033d4dc7b65fdc85d3139445788ab265aa4
Sneha-Sardesai <sneha.sardesai86@gmail.com>
2026-01-18 12:46:09 +0530

    Add message fetching to Home component
    Fetch and display a message from the backend API.

--- gym-frontend/src/components/home.js
+++ gym-frontend/src/components/home.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import gym_pic from "../images/gym_pic.jpg";
 
-
-
 const Home=()=>{
+
+    const [msg, setMsg] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:3001/api/message')
+        .then(res => res.json())
+        .then(data => setMsg(data.message))
+        .catch(err => console.error(err));
+    }, []);
+
     return(
         <div className='homepic' style={{ backgroundImage: `url(${gym_pic})` }}
->        <div className='hero'>
-     <div className='tagline'>Smart Training. Real Results.</div>
-        <button className='start'>Start Workout ---></button>
+>           <div className='hero'>
+            <div className='tagline'>Smart Training. Real Results.</div>
+            <div className='backend-msg' style={{ color: 'white' }}> {msg} </div>
+            <button className='start'>Start Workout --- </button>
 </div>
        
         </div>
     );
-
 }
 
 export default Home;
