0538d797102ebbe8480d2f6623d50e93b01592eb
Sneha-Sardesai <sneha.sardesai86@gmail.com>
2026-01-20 13:37:25 +0530

    backend to mongodb link

--- database/seed.js
+++ database/seed.js
@@ -1,64 +1,26 @@
 // Switch to gym database
-db = db.getSiblingDB("gymDB")
-// Membership Plans
-db.plans.insertMany([
-  {
-    name: "Monthly",
-    price: 1500,
-    durationDays: 30,
-    features: ["Gym Access", "Basic Support"]
-  },
-  {
-    name: "Quarterly",
-    price: 4000,
-    durationDays: 90,
-    features: ["Gym Access", "Trainer Support"]
-  },
-  {
-    name: "Yearly",
-    price: 14000,
-    durationDays: 365,
-    features: ["All Access", "Personal Trainer"]
-  }
-]);
 
-// Trainers
-db.trainers.insertMany([
-  {
-    name: "Rahul",
-    specialization: "Strength Training",
-    experience: 5,
-    active: true
-  },
-  {
-    name: "Anita",
-    specialization: "Cardio & Weight Loss",
-    experience: 3,
-    active: true
-  }
-]);
+db = db.getSiblingDB("gymDB")
 
 // Members
+
 db.members.insertMany([
   {
     name: "Arjun",
     age: 22,
-    plan: "Monthly",
-    trainer: "Rahul",
     active: true,
     joinedOn: new Date()
   },
   {
     name: "Sara",
     age: 25,
-    plan: "Quarterly",
-    trainer: "Anita",
     active: true,
     joinedOn: new Date()
   }
 ]);
 
 // Workout Logs
+
 db.workoutLogs.insertMany([
   {
     memberName: "Arjun",
